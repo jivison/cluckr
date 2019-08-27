55444dd075f0b4e846c2e9aac0f5ca1d9a3c2fdb
John Ivison <john@ivison.org>
2019-08-27 09:57:16 -0700

    Add sign in/out

--- app.js
+++ app.js
@@ -2,18 +2,41 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 
 // Helper functions
 //
 
 const app = express();
 
-app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use((req, res, next) => {
+    res.locals.username = req.cookies.username;
+    next();
+});
 app.use(express.static(path.join(__dirname, "public")));
+app.use(morgan("dev"));
+
+app.set("view engine", "jade");
 
 app.get("/", (req, res) => {
-    res.send("Up and running!")
+    res.cookie("previousPage", "/")
+    res.render("home");
+});
+
+app.get("/sign_in", (req, res) => {
+    res.render("signIn");
+});
+
+app.post("/sign_in", (req, res) => {
+    res.cookie("username", req.body.username);
+    res.redirect((req.cookies.previousPage) ? req.cookies.previousPage : "/");
+});
+
+app.post("/sign_out", (req, res) => {
+    res.clearCookie("username");
+    res.redirect((req.cookies.previousPage) ? req.cookies.previousPage : "/");
 });
 
 const PORT = 5665;
